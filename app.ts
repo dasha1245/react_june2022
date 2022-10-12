3cc98f3ebc7c04f75190937741f10cb9f6b1e9f8
dasha1245 <dcherkasova75@gmail.com>
2022-10-12 16:00:35 +0300

    typescript????

--- app.ts
+++ app.ts
@@ -1,46 +1,95 @@
 // 1) створити інтерфейс на основі цього объекта:
 //     Зверніть увагу там де масиви... в них може бути багато однотипних обїектів
 //
-export interface IObject {
+
+
+const object:IObject<ILinks, IRocket<ICore, IPayload>> = {
+    'mission_name': 'Starlink-15 (v1.0)',
+    'launch_date_local': '2020-10-24T11:31:00-04:00',
+
+    'launch_site': {
+    'site_name_long': 'Cape Canaveral Air Force Station Space Launch Complex 40'
+},
+
+    'links': {
+    'article_link': null,
+        'video_link': 'https://youtu/J442-ti-Dhg'
+},
+
+    'rocket': {
+    'rocket_name': 'Falcon 9',
+        'first_stage': {
+        'cores': [
+            {
+                'flight': 7,
+                'core': {
+                    'reuse_count': 6,
+                    'status': 'unknown'
+                }
+            }
+        ]
+    },
+    'second_stage': {
+        'payloads': [
+            {
+                'payload_type': 'Satellite',
+                'payload_mass_kg': 15400,
+                'payload_mass_lbs': 33951.2
+            }
+        ]
+    }
+}
+}
+
+ interface ILinks{
+    article_link:null | string
+    video_link:string
+}
+
+interface ICore {
+    flight: number
+    core: {
+        reuse_count: number
+        status: string
+    }
+}
+
+interface IPayload {
+    payload_type: string
+    payload_mass_kg: number
+    payload_mass_lbs: number
+}
+
+interface IRocket<ICore, IPayload>{
+    rocket_name: string
+    first_stage: {
+        cores: ICore[]
+    }
+    second_stage: {
+        payloads: IPayload[]
+    }
+}
+
+export interface IObject<ILinks, IRocket> {
     mission_name: string
     launch_date_local: string
     launch_site: {
     site_name_long: string
 }
-    links: {
-    article_link: null
-        video_link: string
-}
-    rocket: {
-    rocket_name: string
-        first_stage: {
-        cores: [
-            {
-                flight: number
-                core: {
-                    reuse_count: number
-                    status: string
-                }
-            }
-        ]
-    }
-    second_stage: {
-        payloads: [
-            {
-                payload_type: string
-                payload_mass_kg: number
-                payload_mass_lbs: number
-            }
-        ]
-    }
-}
+    links: ILinks
+    rocket: IRocket
 }
 //-----------------------------------------------------------------------------------
 
 // 2) протипізувати функції:
 //
-    const user: {name: string, age: number, gender:string} = {
-        name:"Max",
+interface IUser {
+    name: string
+    age: number
+    gender:string
+}
+    const someUser: IUser = {
+        name:'Max',
         age:18,
         gender:'male'
     }
@@ -53,11 +102,11 @@ function showSum(a: number,b: number):void{
     console.log(a + b);
 }
 //
-function incAge(someUser:{age:number}, inc:number):{}{
+function incAge(someUser:IUser, inc:number):{}{
     someUser.age+=inc
     return someUser
 }
 //
 console.log(sum(1, 2));
 showSum(2,3)
-incAge(user, 2)
+incAge(someUser, 2)
